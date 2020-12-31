4c6f3831423ae8e9e626fa63ebec7e20d260984e
Hunter Wiese <whwiese@berkeley.edu>
2020-12-31 13:14:48 -0500

    first used version of training loop

--- train.py
+++ train.py
@@ -10,24 +10,24 @@ from utils import (evaluateModel, plotMSEs)
 import time
 
 #model hyperparameters
-INPUT_DIMS = (3,66,200)
+INPUT_DIMS = (3,455,256)
 
 #other hyperparameters
 LEARNING_RATE = 2e-5
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 10
-WEIGHT_DECAY = 0
+BATCH_SIZE = 64
+WEIGHT_DECAY = 0.01
 EPOCHS = 200
-NUM_WORKERS = 2
-PIN_MEMORY = False
-LOAD_MODEL = False
+NUM_WORKERS = 32
+PIN_MEMORY = True
+LOAD_CHECKPOINT = False
 DROP_LAST = False
-TRAIN_DATA = "data/mini_drive1_train.csv"
-VAL_DATA = "data/mini_drive1_val.csv"
+TRAIN_DATA = "drive/MyDrive/DLData/SteeringAngle/drive1_train.csv"
+VAL_DATA = "drive/MyDrive/DLData/SteeringAngle/drive1_val.csv"
 TEST_DATA = None 
-SAVE_MODEL_PATH = "saved_models/mini_trial1"
-LOAD_MODEL_PATH = None
-IMG_DIR = "data/drive1"
+SAVE_CHECKPOINT_PATH = "trial1/checkpoint"
+LOAD_CHECKPOINT_PATH = "trial1/checkpoint"
+IMG_DIR = "drive1"
 
 class Compose(object):
     def __init__(self, transforms):
@@ -39,7 +39,9 @@ class Compose(object):
 
         return img
 
-transform = Compose([transforms.Resize(INPUT_DIMS[1:]), transforms.ToTensor()])
+#transforms.Resize(INPUT_DIMS[1:])
+
+transform = Compose([transforms.ToTensor()])
 
 def train_fn(train_loader, model, optimizer, loss_fn):
     
@@ -54,17 +56,28 @@ def train_fn(train_loader, model, optimizer, loss_fn):
         loss.backward()
         optimizer.step()
 
-    print("Mean loss: %f"%(sum(mean_loss)/len(mean_loss)))
-
 def main():
     model = CNNDriver(input_dims=INPUT_DIMS).to(DEVICE)
     optimizer = optim.Adam(
             model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
     )
-    loss_fn = nn.MSELoss() 
+    loss_fn = nn.MSELoss(reduction='mean') 
 
-    if LOAD_MODEL:
-        model.load_state_dict(torch.load(LOAD_MODEL_PATH))
+    epochs_passed = 0
+    val_mses = []
+    train_mses = []
+    epochs_recorded = []
+
+    if LOAD_CHECKPOINT:
+        checkpoint = torch.load(LOAD_CHECKPOINT_PATH)
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        for param_group in optimizer.param_groups:
+          param_group['lr'] = LEARNING_RATE
+        epochs_passed = checkpoint['epoch']
+        train_mses = checkpoint['train_mses']
+        val_mses = checkpoint['val_mses']
+        epochs_recorded = checkpoint['epochs_recorded']
 
     train_dataset = SteeringAngleDataset(
             TRAIN_DATA, img_dir=IMG_DIR, transform=transform, 
@@ -109,10 +122,6 @@ def main():
         )
 
 
-    epochs_passed = 0
-    val_mses = []
-    train_mses = []
-    epochs_recorded = []
 
     for epoch in range(EPOCHS):
         if epochs_passed%1 == 0:
@@ -130,9 +139,17 @@ def main():
             print("Train MSE: %f"%(train_mse))
             print("Val MSE: %f"%(val_mse))
 
-        if epochs_passed%10 == 0:
-            save_path = SAVE_MODEL_PATH + ("_%de"%(epochs_passed))+".pt"
-            torch.save(model.state_dict(),save_path)
+        if epochs_passed%5 == 0:
+            save_path = SAVE_CHECKPOINT_PATH + ("checkpoint_%de"%(epochs_passed))+".pt"
+            checkpoint = { 
+                'epoch': epochs_passed,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'train_mses': train_mses,
+                'val_mses': val_mses,
+                'epochs_mses': epochs_recorded,
+            }
+            torch.save(checkpoint,save_path)
             print("Trained for %d epochs"%(epochs_passed))
 
         train_fn(train_loader, model, optimizer, loss_fn)
