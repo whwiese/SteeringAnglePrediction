3080180afb0bfeb8e928e2ae1efc23c5c7a47818
Hunter Wiese <whwiese@berkeley.edu>
2021-01-01 17:48:14 -0500

    Tested First Model Performance

--- utils.py
+++ utils.py
@@ -64,3 +64,60 @@ def plotMSEs(train_mses, val_mses, epochs):
     plt.legend(loc="upper right")
 
     plt.show()
+
+
+def testModel(model, test_loader, loss_fn, 
+        title="Model vs Human Steering Wheel Angles", device="cpu"):
+    """
+    plots model outputs vs human steering angles, returns loss of model
+    outputs and loss of always outputting 0
+    """
+    model.eval()
+
+    zero_loss_sum = 0.0
+    model_loss_sum = 0.0
+
+    test_batches = 0
+    model_preds = []
+    human_angles = []
+
+    for batch_index, (x, y) in enumerate(test_loader):
+        x, y = x.to(device), y.to(device)
+        out = model(x)
+        model_loss = loss_fn(out, y)
+        zero_loss = loss_fn(torch.zeros_like(out), y)
+        model_loss_sum += model_loss.item()
+        zero_loss_sum += zero_loss.item()
+        test_batches += 1
+
+        #record data for plotting
+        model_preds += out.tolist()
+        human_angles += y.tolist()
+
+    zero_loss = zero_loss_sum/test_batches
+    model_loss = model_loss_sum/test_batches
+
+    #plot preds vs human angles
+    fig = plt.figure()
+    ax = plt.axes()
+
+    frames = range(len(model_preds))
+
+    preds = ax.plot(frames, model_preds,
+            color='orange', label="Model"
+    )
+    human = ax.plot(frames, human_angles,
+            color='blue', label="Human Driver"
+    )
+
+    plt.title(title)
+    plt.xlabel('Frame #')
+    plt.ylabel('Steering Wheel Angle (degrees)')
+
+    plt.legend(loc="upper right")
+
+    plt.show()
+
+    model.train()
+
+    return model_loss, zero_loss
